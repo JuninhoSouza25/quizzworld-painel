dc0f24518169431fdc5a29e402c34b34ee4c67bb
EUGENIO PACELLI JUNIOR <dnt-bh@hotmail.com>
2023-10-13 21:39:20 -0300

    feat: eraseuser created

--- src/app/(admin-routes)/admin/components/UserAdmin.js
+++ src/app/(admin-routes)/admin/components/UserAdmin.js
@@ -1,18 +1,22 @@
 'use client'
 
+import PopUp from "@/app/components/PopUp"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import {LiaUserEditSolid} from 'react-icons/lia'
+import {RiDeleteBin2Line} from 'react-icons/ri'
 
 const UserAdmin = () =>{
   const [ allUsers, setAllUsers ] = useState([])
+  const [popup, setPopUp] = useState(false)
+  const [user, setUser] = useState()
 
+  const URL = process.env.URL_API
 
   useEffect(() => {
-    axios.get(`http://localhost:3001/api/users`)
+    axios.get(`${URL}/users`,)
     .then(response => {
       setAllUsers(response.data)
-
     })
     .catch(error => {
       console.log(error)
@@ -20,10 +24,26 @@ const UserAdmin = () =>{
 
   },[])
 
+
+  // Delete User
+  function eraseUser(id){
+    axios.delete(`${URL}/user/${id}`)
+    setAllUsers(allUsers.filter(user => user._id !== id))
+    popup && setPopUp(false)
+  }
+
+  function handlePopUp(userPopup){
+    !popup && setPopUp(true)
+    setUser(userPopup)
+
+  }
+
   
 
   return(
     <div className="user-admin col bg-white shadow">
+
+    {popup && <PopUp user={user} function1={() => eraseUser(user._id)} function2={handlePopUp}/>}
 
       <ul>
         <li className="mb-3 row pt-4 pb-2">
@@ -39,11 +59,14 @@ const UserAdmin = () =>{
           <div className="col-2 fw-light">{item.username}</div>
           <div className="col-3 fw-light">{item.email}</div>
           <div className="col-2 fw-light">{item.role}</div>
-          <div className="col-2 fw-light"><LiaUserEditSolid className="fs-2"/></div>
+          <div className="col-2 fw-light row">
+            <LiaUserEditSolid className="col-2 fs-2 cursor-pointer"/>
+            <RiDeleteBin2Line className="col-2 fs-2 text-danger cursor-pointer" onClick={() => handlePopUp(item)}/>
+          </div>
+
         </li>
       ))}
-      </ul>
-      
+      </ul>      
     </div>
   )
 }
